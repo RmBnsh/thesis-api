55a4662cf706fed820d5d9b2b9c5aa264df2cd82
RmBnsh <brama@uth.gr>
2023-04-06 01:43:05 +0300

    theTitle2

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <nav>
-          thesis obd
+          Thesis obd by Bani and Bak
         </nav>
 
         {children}
